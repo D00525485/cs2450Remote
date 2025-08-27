dbc3dd112cf578a34c35ad0132ff4799b4ea7542
D00525485 <d00525485@utahtech.edu>
2025-08-27 10:41:09 -0600

    made code more efficient

--- age.py
+++ age.py
@@ -1,11 +1,9 @@
 import random
-print("I am going to try and guess your age")
-name = input("What is your name?: ")
+name = input(f"I am going to try and guess your age\nWhat is your name?: ")
 while True:
-	guess = str(random.randint(15,30))
-	check = input(f"Your age is: {guess}\nWas I right?(Y/N)")
+	check = input(f"Your age is: {(x:=random.randint(15,30))}\nWas I right?(Y/N)")
 	if check.lower() != "y":
 		print("rats")
 	else:
-		print(f"{name} is {guess} years old")
+		print(f"{name} is {x} years old")
 		break
